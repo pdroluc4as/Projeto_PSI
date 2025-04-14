7a2002c095f11715ef037ad3cc197e343a09898c
Pedro Lucas Oliveira de Sousa <20231011110035@ifrn.local>
2025-04-14 15:29:10 -0300

    configurações finais

--- lojaAdmin/settings/settings.py
+++ lojaAdmin/settings/settings.py
@@ -1,5 +1,3 @@
-import os
-
 """
 Django settings for lojaAdmin project.
 
@@ -13,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
