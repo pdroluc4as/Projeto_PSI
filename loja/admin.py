ddcf0006962c384482c17488aa799db25e5f6355
Pedro Lucas Oliveira de Sousa <20231011110035@ifrn.local>
2025-05-06 15:13:57 -0300

    modificando painel administrativo

--- loja/admin.py
+++ loja/admin.py
@@ -3,6 +3,17 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(Fabricante)
+class FabricanteAdmin(admin.ModelAdmin):
+    # cria um filtro de hierarquia com datas
+    date_hierarchy = 'criado_em'
+    
+class ProdutoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria') # formata a tabela de forma diferente
+    empty_value_display = 'Vazio' # ficara escrito vazio nos campos que podem ser vazios
+    search_fields = ('Produto',) # permite que eu crie um campo de pesquisa em que vai pesquisar utilizando o que está entre aspas
+
+
+admin.site.register(Fabricante, FabricanteAdmin)
 admin.site.register(Categoria)
-admin.site.register(Produto)
+admin.site.register(Produto, ProdutoAdmin)
