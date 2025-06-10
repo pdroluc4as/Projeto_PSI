e892def01cbc55b5303ae5eb91703ac97ca2104f
Pedro Lucas Oliveira de Sousa <20231011110035@ifrn.local>
2025-06-10 14:22:04 -0300

    função e template de editar produto adicionado

--- loja/urls/ProdutoUrls.py
+++ loja/urls/ProdutoUrls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from loja.views.ProdutoView import list_produto_view
+from loja.views.ProdutoView import list_produto_view, edit_produto_view, edit_produto_postback
 
 urlpatterns = [
     path('', list_produto_view, name='produtos'),
     path('<int:id>', list_produto_view, name='produto'),
+    path('edit/<int:id>', edit_produto_view, name='edit_produto'),
+    path('edit', edit_produto_postback, name='edit_produto_postback'),
 ]
