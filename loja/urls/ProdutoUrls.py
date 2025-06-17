6aea8dd0091308a4404d7aaa95b758954794b3bf
pdroluc4as <pedrolucasoliverdesousa@gmail.com>
2025-06-16 23:00:07 -0300

    criação, edição e exclusão de produtos adicionadas

--- loja/urls/ProdutoUrls.py
+++ loja/urls/ProdutoUrls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from loja.views.ProdutoView import list_produto_view, edit_produto_view, edit_produto_postback, details_produto_view
+from loja.views.ProdutoView import list_produto_view, edit_produto_view, edit_produto_postback, details_produto_view, delete_produto_view, delete_produto_postback, create_produto_view
 
 urlpatterns = [
     path('', list_produto_view, name='produtos'),
@@ -7,4 +7,7 @@ urlpatterns = [
     path('edit/<int:id>', edit_produto_view, name='edit_produto'),
     path('edit/', edit_produto_postback, name='edit_produto_postback'),
     path('details/<int:id>', details_produto_view, name='produto_details'),
+    path('delete/<int:id>', delete_produto_view, name='produto_delete'),
+    path('delete/', delete_produto_postback, name='produto_delete_postback'),
+    path('create/', create_produto_view, name='produto_create'),
 ]
