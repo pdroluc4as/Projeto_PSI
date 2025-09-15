03cf32849c45e24a9c7e7a988cda3d75ccbb538f
pdroluc4as <pedrolucasoliverdesousa@gmail.com>
2025-09-15 15:42:47 -0300

    adicionado função de login e registrar
    capitulo 10 concluido

--- lojaAdmin/settings/settings.py
+++ lojaAdmin/settings/settings.py
@@ -126,3 +126,10 @@ MEDIA_ROOT=os.path.join(BASE_DIR, 'media') # é quem lida com a mídia vinculado
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Login
+LOGIN_URL = '/login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_URL = '/logout'
+LOGOUT_REDIRECT_URL = '/login'
